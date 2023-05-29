f6d9b1ad9794acda120ed2cdcac4263c823b85c0
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-05-29 09:23:57 +0800

    update

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,55 +1,72 @@
 import "./Register.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Form, Button } from "../../UI";
 import { useState } from "react";
-import axios from "axios"
+import axios from "axios";
 
 const Register = () => {
   const [inputs, setInputs] = useState({
     name: "",
     email: "",
-    password: ""
-  })
+    password: "",
+  });
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setInputs((prev) => {
-      return ({...prev, [e.target.name]:e.target.value})
-    })
-  }
-
-  console.log(inputs)
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      const res = await axios.post(
-        "http://localhost:5000/api/auth/register",
-        inputs
-      );
-      console.log(res)
+      await axios.post("http://localhost:5000/api/auth/register", inputs);
+      navigate("/");
     } catch (error) {
-      console.log(error)
+      setError(error?.response?.data);
     }
-
-  }
+  };
 
   return (
     <>
       <Form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" onChange={handleChange} required/>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={handleChange}
+            required
+          />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" onChange={handleChange} required/>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={handleChange}
+            required
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" onChange={handleChange} required/>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handleChange}
+            required
+          />
         </div>
         <Button type="submit">Register</Button>
+        {error && <p>{error}</p>}
         <p>
           Already have an account? <Link to="/login">Login here</Link>
         </p>
